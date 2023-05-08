1c87abdfb0d7eb4f18f36a015b5713edc9df147c
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2023-05-08 18:26:44 -0400

    added mouseover event on img and did finishing touch on webiste

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const h2 = document.createElement('h2');
 h2.textContent = "WELCOME TO ZEEKS WEBSITE!!";
 
 document.querySelector("body").appendChild(h2);
+
+function newPicture(){
+
+    document.getElementById("image").src="IMG_1098.JPG";
+
+}
+function oldPicture(){
+
+    document.getElementById("image").src="IMG_1108.JPG";
+}
+    
