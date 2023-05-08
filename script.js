5a99f5e9e6650855d99acdf5935bf12ca223a1a2
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2023-05-08 17:35:25 -0400

    added img to html, changed look in css  and made changes to script.js h2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement('h2');
-h2.textContent = "This constent added by Javascript";
+h2.textContent = "WELCOME TO ZEEKS WEBSITE!!";
 
 document.querySelector("body").appendChild(h2);
